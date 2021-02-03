55a1bf283cb764317603693a89fb5eb3eb1cb6b9
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-03 18:22:06 +0000

    Adding tests for dock and bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,4 +8,14 @@ describe DockingStation do
     expect(subject.release_bike).to be_working
   end
 
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it "returns a docked bike" do
+  bike = Bike.new
+  subject.dock(bike)
+  expect(subject.bike).to eq bike
+  end
 end
