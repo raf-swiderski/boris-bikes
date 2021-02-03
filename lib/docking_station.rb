ee70a89bdee0dcdd638c090266560d8a881795e8
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-03 18:21:19 +0000

    Adding dock and bike method with attribute reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,8 +3,20 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+  def bike
+    @bike
+  end
+
+
 
 end
