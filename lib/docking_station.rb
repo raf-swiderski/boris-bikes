dcd835428c4c1d71a9754ec9d71af27e0582e80a
raf <rafswid@icloud.com>
2021-02-03 17:24:00 +0000

    chapter 10

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,10 @@
 # in lib/docking_station.rb
+require_relative 'bike'
+
 class DockingStation
 
   def release_bike
+    Bike.new
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,8 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:release_bike) }
 
+  it 'releases a working bike' do
+    expect(subject.release_bike).to be_working
+  end
+
 end
